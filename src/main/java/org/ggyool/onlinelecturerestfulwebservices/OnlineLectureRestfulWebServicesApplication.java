06383a958f66114b021d71ab9f8f8804056adf01
seunghyun <ggyool@naver.com>
2022-01-07 19:38:43 +0900

    initial commit

---
+++ src/main/java/org/ggyool/onlinelecturerestfulwebservices/OnlineLectureRestfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package org.ggyool.onlinelecturerestfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLectureRestfulWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineLectureRestfulWebServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ggyool/onlinelecturerestfulwebservices/OnlineLectureRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ggyool.onlinelecturerestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLectureRestfulWebServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
